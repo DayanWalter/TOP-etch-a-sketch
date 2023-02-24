460e8f142acdc1bf57db79f66e74d06fc070dbb7
SynthCyraX <SynthCyraX@gmail.com>
2023-02-24 05:50:45 +0100

    Added if statement if number is too high or low

--- javascript.js
+++ javascript.js
@@ -7,13 +7,21 @@ function createRowsAndColums(){
     // set everything to default
     container.innerHTML = "";
     let colums = prompt("Enter number of colums","");
-    // loop through the prompt input and create divs
-    for(i=0; i<(colums*colums); i++){
-        const div = document.createElement("div");
-        container.appendChild(div);
+    // check if the input is to high or to low
+    if(colums >=1 && colums <= 100){
+        // loop through the prompt input and create divs
+        for(i=0; i<(colums*colums); i++){
+            const div = document.createElement("div");
+            container.appendChild(div);
+        }
+    }else if(colums<=0){
+        alert("Input too low!")
+    }else{
+        alert("Input too high!")
     }
-    // define how much rows and colums are in the container, dependend on the prompt input
-    container.style.gridTemplateColumns = `repeat(${colums}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${colums}, 1fr)`;
+        // define how much rows and colums are in the container, dependend on the prompt input
+        container.style.gridTemplateColumns = `repeat(${colums}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${colums}, 1fr)`;
 }
+
 
