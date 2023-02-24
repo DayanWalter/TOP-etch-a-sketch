df4517cfca0c688df40524eb1ed9468552cffc53
SynthCyraX <SynthCyraX@gmail.com>
2023-02-24 07:07:29 +0100

    Added random color generator

--- javascript.js
+++ javascript.js
@@ -11,17 +11,27 @@ function createRowsAndColums(){
     if(colums >=1 && colums <= 100){
         // loop through the prompt input and create divs
         for(i=0; i<(colums*colums); i++){
-            const div = document.createElement("div");
-            container.appendChild(div);
+            const addedDiv = document.createElement("div");
+            addedDiv.classList = "addedDiv";
+            // addEventlister here, NOT outside of the function
+            addedDiv.addEventListener('mouseover', hover);
+            container.appendChild(addedDiv);
         }
-    }else if(colums<=0){
+    }else if(colums <= 0){
         alert("Input too low!")
     }else{
         alert("Input too high!")
     }
-        // define how much rows and colums are in the container, dependend on the prompt input
-        container.style.gridTemplateColumns = `repeat(${colums}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${colums}, 1fr)`;
+    // define how much rows and colums are in the container, dependend on the prompt input
+    container.style.gridTemplateColumns = `repeat(${colums}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${colums}, 1fr)`;
 }
 
+// add class to the div via mouseover
+function hover () {
+    const r = Math.floor(Math.random() * 256); 
+    const g = Math.floor(Math.random() * 256); 
+    const b = Math.floor(Math.random() * 256); 
+    this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
 
